9c0c38c5e8c678e0bda783a7d5b1d1fb94666954
vladuxa23 <vlad_1995.02@mail.ru>
2022-02-15 18:20:18 +0300

    add dynamic link and add data handlers on backend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,4 +6,23 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html", title="Hello", data="Первая страница сайта")
+
+
+@app.route("/handler", methods=['GET', 'POST'])
+def handler():
+    if request.method == 'POST':
+        return f"<h1>Hello {request.form['name']}</h1>"
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route("/profile/<string:name>", methods=['GET', 'POST'])
+def profile(name):
+    if request.method == 'POST':
+#
+       return f"<h1>Hello {name}</h1>"
+#     else:
+#         return redirect(url_for('index'))
+    else:
+        return f"<h1>Hello {name}</h1>"
 
